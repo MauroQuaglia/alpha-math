d3be76077509e2d4d3d73283e0792d7f11805254
nautilus@porpoise <nautilus@trovaprezzi.it>
2019-02-21 17:33:02 +0100

    Prime.

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -2,11 +2,22 @@ require_relative '../lib/mathematicus/prime'
 
 describe Prime do
 
-  it 'should return false' do
-    expect(described_class.prime?(0)).to be_falsey
-    expect(described_class.prime?(1)).to be_falsey
-    expect(described_class.prime?(2)).to be_truthy
-    expect(described_class.prime?(3)).to be_truthy
+  [-1, 0].each do |value|
+    it "#{value} should not be prime" do
+      expect(described_class.prime?(value)).to be_falsey
+    end
+  end
+
+  [1, 4, 6, 8, 9, 10].each do |value|
+    it "#{value} should not be prime" do
+      expect(described_class.prime?(value)).to be_falsey
+    end
+  end
+
+  [2, 3, 5, 7].each do |value|
+    it "#{value} should be prime" do
+      expect(described_class.prime?(value)).to be_truthy
+    end
   end
 
 end
