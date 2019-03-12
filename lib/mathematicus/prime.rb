0ae66c6e45a4bf5e25ba2e82545b530e8517d6dc
nautilus@narwhal <nautilus@trovaprezzi.it>
2019-03-12 15:57:50 +0100

    decomposition.

--- lib/mathematicus/prime.rb
+++ lib/mathematicus/prime.rb
@@ -7,4 +7,17 @@ class Prime
     end
   end
 
+  def self.decomposition(number, factors = [])
+    return factors if number == 1
+    (2..Math.sqrt(number).to_i).each do |divisor|
+      factors << divisor if number.divmod(divisor).last.zero?
+      decomposition(number.divmod(divisor).first, factors)
+    end
+  end
+
 end
+
+
+#Primes in numbers
+#
+# xample: n = 86240 should return "(2**5)(5)(7**2)(11)"
--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -2,22 +2,40 @@ require_relative '../lib/mathematicus/prime'
 
 describe Prime do
 
-  [-1, 0].each do |value|
-    it "#{value} should not be prime" do
-      expect(described_class.prime?(value)).to be_falsey
+  describe '.prime?' do
+
+    [-1, 0].each do |value|
+      it "#{value} should not be prime" do
+        expect(described_class.prime?(value)).to be_falsey
+      end
     end
+
+    [1, 4, 6, 8, 9, 10].each do |value|
+      it "#{value} should not be prime" do
+        expect(described_class.prime?(value)).to be_falsey
+      end
+    end
+
+    [2, 3, 5, 7].each do |value|
+      it "#{value} should be prime" do
+        expect(described_class.prime?(value)).to be_truthy
+      end
+    end
+
   end
 
-  [1, 4, 6, 8, 9, 10].each do |value|
-    it "#{value} should not be prime" do
-      expect(described_class.prime?(value)).to be_falsey
-    end
-  end
+  describe '.decomposition' do
 
-  [2, 3, 5, 7].each do |value|
-    it "#{value} should be prime" do
-      expect(described_class.prime?(value)).to be_truthy
+    [2, 3, 5, 7, 11, 13, 17, 19].each do |value|
+      it "#{value} should not be decomposed" do
+        expect(described_class.decomposition(value)).to eq("(#{value})")
+      end
     end
+
+    it "4 should be decomposed" do
+      expect(described_class.decomposition(4)).to eq([2, 2])
+    end
+
   end
 
 end
