da89955ea79021f4b62d8fc55de99e1da307de94
nautilus@porpoise <nautilus@trovaprezzi.it>
2019-09-18 17:26:47 +0200

    fix.

--- lib/mathematicus/digital.rb
+++ lib/mathematicus/digital.rb
@@ -1,9 +1,9 @@
 class Digital
 
   def self.root(number)
-    return 0 if number.zero?
+    return 0 if number == 0
     rest = number.divmod(9).last
-    rest.zero? ? 9 : rest
+    rest == 0 ? 9 : rest
   end
 
 end
